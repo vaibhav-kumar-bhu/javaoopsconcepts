0076a3797743b7eacdb5c826430752ea0c0e730d
satyajeet kumar <satyajeetkumar499@gmail.com>
2021-03-17 14:14:49 +0530

    first

---
+++ SatyajeetKumar/Basic_Java_Coding/Hello_World.java
@@ -0,0 +1,8 @@
+public class Hello_World {
+    // int score = 4;
+    public static void main(String args[]) {
+         int score = 1;
+      
+                System.out.println("Hello World");
+        }
+    }
