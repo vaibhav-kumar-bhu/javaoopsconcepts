39062159c201d3e3b2c15b4234efd14200a81e5b
satyajeet kumar <satyajeetkumar499@gmail.com>
2021-03-18 15:16:41 +0530

    commit

--- Javacodes/satyajeet/Basic_Code/Main.java
+++ Javacodes/satyajeet/Basic_Code/Main.java
@@ -3,17 +3,32 @@ import java.util.*;
 import java.lang.*;
 
  class Accessmodifiers{
-	protected void method()
+ 	// public methods access anywhere
+	public void method()
 	{
 		 System.out.println("Access");
 	}
+    
+    // private methods access only in same class 
+    private void method1()
+	{
+		 System.out.println("Access");
+	}
+    
+    // protected methods access package and it's subclass.
+    protected void method2()
+	{
+		 System.out.println("Access");
+	}
+
 }
-public class Main extends Accessmodifiers {
+public class Main {
     
-    
+     
     public static void main(String args[]) {
          
          Accessmodifiers obj=new Accessmodifiers();
          obj.method();
+         obj.method2();
     }
 }
--- Javacodes/satyajeet/Basic_Code/StaticandnonStaticMethod.java
+++ Javacodes/satyajeet/Basic_Code/StaticandnonStaticMethod.java
@@ -2,18 +2,25 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 
- class Accessmodifiers{
-	protected void method()
-	{
-		 System.out.println("Access");
-	}
-}
-public class StaticandnonStaticMethod extends Accessmodifiers {
+public class StaticandnonStaticMethod {
     
-    
+      // this is static method
+      public static void staticmethod()
+      {
+      	  System.out.println("static method");
+      }
+     
+     //  this is non static method
+      public void nonstaticmethod()
+      {
+      	System.out.println("Non static metod");
+      }
     public static void main(String args[]) {
          
-         Accessmodifiers obj=new Accessmodifiers();
-         obj.method();
+         StaticandnonStaticMethod nonstaticObj=new StaticandnonStaticMethod();
+         // static method no need to have an object
+         staticmethod();
+         //  non static method need to have object
+         nonstaticObj.nonstaticmethod();
     }
 }
